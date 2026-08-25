d25ff003f882146f4b34f614f74529fc7f5d0175
你的名字 <2879677705@qq.com>
2026-08-25 10:42:17 +0800

    selenium的项目操作

--- web自动化/Test.py
+++ web自动化/Test.py
@@ -1,13 +1,26 @@
 #导包
 from selenium import webdriver
 from time import sleep
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 #打开浏览器
-driver=webdriver.Chrome()
+driver = webdriver.Chrome()
 
 #进入百度网站
 driver.get('https://www.baidu.com/')
+sleep(3)
+#查找搜索框元素
+search = driver.find_element(By.NAME, 'wd')
+sleep(3)
+#输入搜索内容
+search.send_keys("Runoob")
+sleep(3)
+#模拟回车
+search.send_keys(Keys.RETURN)
+
 #等待3秒
-sleep(10)
+sleep(3)
 
 #关闭 quit()
 driver.quit()
